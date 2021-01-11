75ac93974da7bf6ab75ed896836ecca8cb729950
Adarsh Shetty <shettyadarsh18@gmail.com>
2021-01-11 09:11:14 +0530

    Added the main script

---
+++ fcmserverkey.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+import subprocess
+import requests
+import json
+import os
+import sys
+from shutil import which
+from colorama import Fore
+
+url = 'https://fcm.googleapis.com/fcm/send'
+body = {"registration_ids": ["ABC"]}
+headers = {"content-type": 'application/json'}
+
+
+# Checking If apktool is installed
+def check_apktool():
+    if which("apktool"):
+        return True
+    else:
+        return False
+
+
+# Decompiling the apk
+def decompile_apk(apk_file):
+    print(Fore.BLUE + f"[+] Decompiling {apk_file}!")
+    os.popen(f"apktool d {apk_file}").read()
+    print(Fore.BLUE + "[+] Apk decompiled!")
+    return apk.split(".")[0]
+
+
+# Extracting the keys using both regex
+def extract_keys(_out_dir):
+    output1 = subprocess.getoutput(f'grep -ProIR "AIzaSy[0-9A-Za-z_-]{{33}}" {_out_dir}')
+    output2 = subprocess.getoutput(f'grep -ProIR "AAAA[A-Za-z0-9_-]{{7}}:[A-Za-z0-9_-]{{140}}" {_out_dir}')
+
+    res = []
+    if output1:
+        res1 = output1.splitlines()
+        res.extend(res1)
+    if output2:
+        res2 = output2.splitlines()
+        res.extend(res2)
+
+    if not res:
+        print(Fore.RED + "[-] No tokens found")
+        exit()
+
+    keys = [i.split(":")[1] for i in res]
+    print(Fore.BLUE + f"[+] Found some tokens: {', '.join(keys)}")
+    return list(set(keys))
+
+
+# Validating the server keys
+def validate_keys(key):
+    headers["Authorization"] = f"key={key}"
+    r = requests.post(url, data=json.dumps(body), headers=headers)
+    if r.status_code == 200:
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    if not check_apktool():
+        print(Fore.RED + "[!] Install apktool for decompiling the apk!")
+        exit()
+    apk = sys.argv[1]
+    out_dir = decompile_apk(apk)
+    server_keys = extract_keys(out_dir)
+    for server_key in server_keys:
+        if validate_keys(server_key):
+            print(Fore.GREEN + f"[+] {server_key} is a valid server key")
+        else:
+            print(Fore.RED + f"[-] {server_key} is not a valid server key")
+    print(Fore.BLUE + f"[+] Finished!")
